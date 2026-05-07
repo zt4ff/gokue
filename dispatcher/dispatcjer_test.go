19729b26c9a5ae034f68d3b55bd52db6275af801
olukayode <segunkayode00@gmail.com>
2026-05-07 15:12:17 +0100

    wip: dispatcher test

--- dispatcher/dispatcjer_test.go
+++ dispatcher/dispatcjer_test.go
@@ -1,1 +1,25 @@
 package dispatcher_test
+
+import (
+	"testing"
+
+	"github.com/zt4ff/gokue/config"
+	"github.com/zt4ff/gokue/dispatcher"
+)
+
+func setup(t *testing.T) *dispatcher.Dispatcher {
+	t.Helper()
+
+	config := config.Config{
+		Backend:   "in-memory",
+		QueueSize: 10,
+	}
+
+	dispatcher := dispatcher.New(config, nil)
+
+	return dispatcher
+}
+
+func TestXxx(t *testing.T) {
+	_ = setup(t)
+}
